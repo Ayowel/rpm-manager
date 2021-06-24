8626812f5a7d61aeb6b1df0130fcb894d0c4448a
Ayowel <Ayowel@users.noreply.github.com>
2021-06-25 00:22:20 +0200

    Add a version upgrade preparation tool

---
+++ tools/prepare-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script updates the files thatcontain a version number
+if test "$#" -ne 1 || ! [[ "$1" =~ [0-9]+\.[0-9]+\.[0-9]+[-a-z]* ]]; then
+  echo 'This script takes a single version number as argument' >&2
+  exit 1
+fi
+
+version_number="$1"
+
+sed -i -e "s/^\\(PROJECT_NUMBER\\s*=\\s*\\).*\$/\\1${version_number}/" Doxyfile
+sed -i -e "0,/^## .*$/s//## Version ${version_number}/" CHANGELOG.md 
+sed -i -e "s/\\(VERSION_NUMBER=\\).*$/\\1${version_number}/g" src/core.sh
